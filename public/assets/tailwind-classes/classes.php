3baa93341042ee983a0a4a55d8346a220e30d34d
journeyofgrey <normiegrey@gmail.com>
2025-08-23 21:47:52 -0800

    1st

---
+++ public/assets/tailwind-classes/classes.php
@@ -0,0 +1,22 @@
+<?php
+
+$loginStyle = [
+    'container' => 'flex items-center justify-center min-h-screen px-4 py-12 sm:px-6 lg:px-8 bg-gray-50',
+    'form_container' => 'rounded-lg shadow-2xl w-full max-w-md bg-white p-8 sm:p-10',
+    'form_input' => 'w-full p-4 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 transition duration-200',
+
+
+
+    'submit_button' => 'w-full p-4 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200',
+
+
+    'forgot_link' => 'text-blue-500 hover:underline focus:outline-none focus:ring-2 focus:ring-blue-300',
+    'heading' => 'text-lg font-semibold text-gray-800 mb-4',
+    'hr' => 'my-4 border-t border-gray-300',
+
+    // Logo and text classes
+    'logo' => 'h-16',  // Adjust the size of the logo
+    'logoWrapper' => 'flex items-center justify-between w-full mb-8',  // Flex container for logo and text
+    'logoTitle' => 'font-bold text-2xl text-gray-800',  // Styling for brand name
+    'logoSubtitle' => 'text-sm text-gray-600',  // Styling for tagline
+];
